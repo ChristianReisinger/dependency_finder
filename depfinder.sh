52127c6c5fe431c797fbdd1e40d09d5dbf3027d6
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-07-31 16:52:40 +0200

    Added optional parameters to specify alternative 'includes' and 'libs' filenames

--- depfinder.sh
+++ depfinder.sh
@@ -2,6 +2,8 @@
 
 project_dir=${1:?"Error: no project directory"}
 src_file=${2:?"Error: no source file"}
+includes_file=${3:-includes}
+libs_file=${4:-libs}
 #src_dir=${src_file%/*}
 #project_dir=${src_dir%/*}
 
@@ -24,7 +26,7 @@ done <<< "$includes"
 declare -A include_dir_used
 while read -r Idir; do
 	include_dir_used["$Idir"]=0
-done < "includes"
+done < "$includes_file"
 
 declare -A lib_files
 while read -r lib; do
@@ -34,7 +36,7 @@ done <<< "$includes"
 declare -A lib_dir_used
 while read -r Ldir; do
 	lib_dir_used["$Ldir"]=0
-done < "libs"
+done < "$libs_file"
 
 #####################################################################################################
 #determine which include / lib directories are needed and which headers / libraries are found in them
@@ -52,7 +54,7 @@ while read -r Idir; do
 			fi
 		fi
 	done <<< "$includes"
-done < "includes"
+done < "$includes_file"
 
 while read -r Ldir; do
 	while read -r lib; do
@@ -68,7 +70,7 @@ while read -r Ldir; do
 			fi
 		fi
 	done <<< "$includes"
-done < "libs"
+done < "$libs_file"
 
 #####################################################################################################
 #print options -I -L -l to be passed to the compiler
@@ -77,13 +79,13 @@ while read -r Idir; do
 	if [ ${include_dir_used["$Idir"]} -eq 1 ]; then
 		printf '%s\0\n' -I"$Idir"
 	fi
-done < "includes"
+done < "$includes_file"
 
 while read -r Ldir; do
 	if [ ${lib_dir_used["$Ldir"]} -eq 1 ]; then
 		printf '%s\0\n' -L"$Ldir"
 	fi
-done < "libs"
+done < "$libs_file"
 
 while read -r lib; do
 	libstem="${lib%.*}"
